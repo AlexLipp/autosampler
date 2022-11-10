b6f5ae723d798d773a8dbe43b36d189cdd7aadd7
Alex Lipp <alex@lipp.org.uk>
2022-11-10 12:53:10 +0000

    Improve runtime using numpy index deletion

--- autosampler.py
+++ autosampler.py
@@ -13,66 +13,9 @@ from matplotlib.colors import LogNorm
 # TODO: Generate a readME
 
 
-def get_sample_nodes_by_number(model_grid, num_samps, increment=0.05):
-
-    print("~~~~~~~ Beginning Calculation ~~~~~~~")
-    print("Assigning every node an upstream area")
-    # Node array contining downstream-to-upstream ordered list of node
-    ordered_nodes = model_grid.at_node["flow__upstream_node_order"]
-    num_non_sink_nodes = sum(np.invert(model_grid.at_node["flow__sink_flag"]))
-    A_per_samp = num_non_sink_nodes / num_samps
-
-    node_area_dict = {}
-    counter = 0
-    for node in ordered_nodes:
-        if counter % 1000 == 0:
-            print("\t Assigning node", counter, "of", len(ordered_nodes))
-        node_area_dict[node] = landlab.utils.get_watershed_nodes(model_grid, node)
-        counter += 1
-    samps_allocated = 0
-    iterations = 0
-    print("Starting iterations")
-    while samps_allocated < num_samps:
-
-        V = []  # visited nodes
-        uV = ordered_nodes.tolist()  # unvisited otes
-        sample_nodes = {}
-        samps_allocated = 0
-        counter = 0
-        print("Trying with target area = ", A_per_samp)
-        print("Looping through all nodes")
-        for i in np.arange(len(ordered_nodes) - 1, -1, -1):
-            if counter % 1000 == 0:
-                print("\t Processing node", counter, "of", len(ordered_nodes))
-            # Iterate from upstream to downstream
-            node = ordered_nodes[i]  # Node in network
-            all_upst_nodes = node_area_dict[node]  # Get all the upstream nodes
-            unvisited_upst_nodes = all_upst_nodes[
-                np.isin(all_upst_nodes, uV)
-            ]  # Identify those which are 'new'
-            if len(unvisited_upst_nodes) > A_per_samp:
-                print("\t Found a sample locality:", node)
-                sample_nodes[node] = unvisited_upst_nodes  # Add node to list
-                for x in unvisited_upst_nodes:  # Remove added nodes to unvisited list
-                    uV.remove(x)
-                samps_allocated += 1
-                if samps_allocated == num_samps:
-                    break
-            counter += 1
-        iterations += 1
-        print("##### Finished iteration", iterations, "#####")
-        if samps_allocated < num_samps:
-            print("Only allocated:", samps_allocated, "of", num_samps)
-            print("Trying again with smaller target area...")
-            print("___________________________")
-        else:
-            print("All samples allocated")
-            print("Finishing...")
-        A_per_samp = A_per_samp * (
-            1 - increment
-        )  # initial target number of nodes per samp
-    print("~~~~~~~ Finished Calculation ~~~~~~~")
-    return sample_nodes
+def fast_delete(x, elements):
+    indices = np.ravel([np.where(x == e) for e in elements])
+    return np.delete(x, indices)
 
 
 def get_sample_nodes_by_area(model_grid, target_area):
@@ -84,7 +27,7 @@ def get_sample_nodes_by_area(model_grid, target_area):
     cell_area = model_grid.dx * model_grid.dy
     nodes_per_samp = target_area / cell_area
 
-    uV = ordered_nodes.tolist()  # unvisited nodes
+    uV = ordered_nodes.copy()  # unvisited nodes
     print("Removing catchments smaller than target area")
     # Remove nodes from uV if they are within catchments smaller than target (as they will never be visited)
     num_sinks = len(np.where(model_grid.at_node["flow__sink_flag"])[0])
@@ -100,13 +43,13 @@ def get_sample_nodes_by_area(model_grid, target_area):
                 datetime.now().strftime("%H:%M:%S"),
             )
         if model_grid.at_node["drainage_area"][sink] < target_area:
-            i=np.where(ordered_nodes==sink)[0][0]
-            bad_nodes=[sink]
-            for up_node in ordered_nodes[i+1:]:
+            i = np.where(ordered_nodes == sink)[0][0]
+            bad_nodes = [sink]
+            for up_node in ordered_nodes[i + 1 :]:
                 if receiver_at_node[up_node] in bad_nodes:
                     bad_nodes.append(up_node)
-                    uV.remove(up_node)
                 else:
+                    uV = fast_delete(uV, bad_nodes)
                     break
         counter += 1
 
@@ -116,7 +59,8 @@ def get_sample_nodes_by_area(model_grid, target_area):
     print("Target area = ", nodes_per_samp)
     print("Looping through all unvisited nodes upstream to downstream")
     initial_len = len(uV)
-    for i in np.arange(len(uV) - 1, -1, -1):
+    visited = 0
+    for i in np.arange(initial_len - 1, -1, -1):
         if counter % 1000 == 0:
             print(
                 "\t Processing node",
@@ -126,7 +70,6 @@ def get_sample_nodes_by_area(model_grid, target_area):
                 ",",
                 datetime.now().strftime("%H:%M:%S"),
             )
-
         node = uV[i]  # Node in network
         unvis_up_nodes = [
             node
@@ -140,12 +83,11 @@ def get_sample_nodes_by_area(model_grid, target_area):
         if len(unvis_up_nodes) > nodes_per_samp:
             print("\t * Found a sample locality *")
             sample_nodes[node] = unvis_up_nodes  # Add node to list
-            for x in unvis_up_nodes:  # Remove added nodes to unvisited list
-                uV.remove(x)
+            uV = fast_delete(uV, unvis_up_nodes)  # Remove visited nodes from list
             samps_allocated += 1
         counter += 1
     print("Found", samps_allocated, "sample localities")
-    area_sizes = [len(areas)*cell_area for _, areas in sample_nodes.items()]
+    area_sizes = [len(areas) * cell_area for _, areas in sample_nodes.items()]
     mean, std = np.mean(area_sizes), np.std(area_sizes)
     print("Average area per basin = ", mean, "+/-", std)
     print("~~~~~~~ Finished Calculation ~~~~~~~")
@@ -159,7 +101,7 @@ def viz_drainage_area(model_grid):
         origin="lower",
     )
     cb = plt.colorbar()
-    cb.set_label("Drainage Area (# of nodes)")
+    cb.set_label("Drainage Area")
     plt.xlabel("x")
     plt.ylabel("y")
 
